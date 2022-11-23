066c873df50f9373959f75fdc758f1d91823cf1e
Tran Hai <khoidaumoichotoi@gmail.com>
2022-11-23 14:59:33 +0700

    moves jazzmin template folder

--- authentication/apps.py
+++ authentication/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
-
+from django.utils.translation import gettext_lazy as _
 
 class AuthenticationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'authentication'
+    name : str = 'authentication'
+    verbose_name : str = _("Authentication")
---
+++ utils/scripts.py
@@ -0,0 +1,39 @@
+from django.contrib.auth import get_user_model
+
+def create_admin_user():
+    try:
+        username = "admin"
+        email = "admin@admin.gmail.com"
+        password = "admin123"
+        User = get_user_model()
+        user = User.objects.create_user(username, email, password)
+        user.check_password(password)
+        user.is_active = 1
+        user.is_staff = 1
+        user.is_superuser = 1
+        user.save()
+        print("Create admin user ... OK")
+    except Exception as ex:
+        print("CREATE ADMIN USER ERROR: ", ex)
+
+def create_user():
+    try:
+        username = "seller1"
+        email = "seller1@.gmail.com"
+        password = "seller1_sell"
+        User = get_user_model()
+        user = User.objects.create_user(username, email, password)
+        user.check_password(password)
+        user.is_active = 0
+        user.is_staff = 0
+        user.is_superuser = 0
+        user.save()
+        print("Create user ... OK")
+    except Exception as ex:
+        print("CREATE USER ERROR: ", ex)
+
+create_admin_user()
+
+
+
+
