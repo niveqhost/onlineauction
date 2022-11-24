27df93d5cc8aee6b81df04a8744583f2174743d4
Tran Hai <khoidaumoichotoi@gmail.com>
2022-11-25 01:01:07 +0700

    update register user feature

--- authentication/views.py
+++ authentication/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from django.contrib import messages
 from django.views import generic
 from django.utils.translation import gettext_lazy as _
 
+from validate_email import validate_email
+from utils import constants
 # Create your views here.
 
 # ----------------------- Nguoi dung dang ki tai khoan -----------------------
@@ -15,17 +18,29 @@ class UserRegister(generic.View):
             print("USER REGISTER GET REQUEST ERROR: ", ex)
     def post(self, request, *args, **kwargs):
         try:
-            context = {'has_error': False, 'data': request.POST}
-            # * Lay thong tin nguoi dung nhap vao tu form
-            username = request.POST.get('register_username')
-            email = request.POST.get('register_email')
-            password = request.POST.get('register_password')
-            repassword = request.POST.get('register_repassword')
-            # * Xac minh va lam sach thong tin, bao mat thong tin
-            self.user_validation(username=username, email=email, password=password, repassword=repassword)
+            context = { 'has_error': False, 'data': request.POST }
+            #* Xac thuc thong tin dang ki cua nguoi dung
+            self.validate_user(request=request, context=context)
+            return render(request, self.template_name, context)
         except Exception as ex:
             print("USER REGISTER POST REQUEST ERROR: ", ex)
 
-    # * Xac minh va lam sach thong tin, bao mat thong tin
-    def user_validation(self, *args, **kargs):
-        print(kargs.get("username"))
+    #* Xac minh va lam sach thong tin, bao mat thong tin
+    def validate_user(self, *args, **kargs):
+        #* Lay thong tin nguoi dung nhap vao tu form
+        request = kargs.get('request')
+        context = kargs.get('context')
+        username = request.POST.get('register_username')
+        email = request.POST.get('register_email')
+        password = request.POST.get('register_password')
+        repassword = request.POST.get('register_repassword')
+        #* Xac thuc mat khau
+        if len(password) < 6:
+            messages.add_message(request, constants.MY_MESSAGE_LEVEL, _("Password should be at least 6 characters."), constants.MY_ERROR_TAG)
+            context['has_error'] = True
+        #* Xac thuc email
+        if not validate_email(email):
+            messages.add_message(request, constants.MY_MESSAGE_LEVEL, _("Enter a valid email address."), constants.MY_ERROR_TAG)
+            context['has_error'] = True
+        print(context)
+        return context
