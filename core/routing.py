7f6c39d29cbb39a663d16bc4577150b8cb8ab3bf
Tran Hai <khoidaumoichotoi@gmail.com>
2022-12-19 16:57:34 +0700

    make some progress on bidding feature

--- core/routing.py
+++ core/routing.py
@@ -2,5 +2,5 @@ from django.urls import path
 from auction import consumers
 
 websocket_urlpatterns = [
-    path('ws/auction', consumers.MySyncConsumer.as_asgi()),
+    path('ws/auction/<int:product_id>/<slug:product_slug>', consumers.MySyncConsumer.as_asgi()),
 ]
